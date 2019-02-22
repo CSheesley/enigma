7f3ee0843a08f1265208a1c92197ecdfb2cbce58
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-22 16:59:34 -0700

    Includes basic set up of enigma class - mapping out possible input and output of encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,15 @@
+require './lib/key'
+require './lib/offset'
+
 class Enigma
+  # include Key
+  # include Offset
+
+  # def encrypt(phrase, key, date)
+  #   encrypted = {
+  #     encryption: phrase,
+  #     key: key,
+  #     date: date}
+  # end
 
 end
