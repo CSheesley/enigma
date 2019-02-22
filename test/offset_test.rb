7396931111091d94665861849e215ff679a1ebb8
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-22 16:15:41 -0700

    Adds offset test skeleton

---
+++ test/offset_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+
+class OffsetTest < Minitest::Test
+
+  def test_it_exists
+    offset = Offset.new
+    assert_equal Offset, offset
+  end
+
+  def test_it_takes_in_a_date_argument
+
+
+  end
+
+
+end
