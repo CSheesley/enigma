1a52124575777a654f49edb5a8e4ed04ceebe964
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-23 10:47:00 -0700

    Adds tests for last four digits squaredm and offset values

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -2,43 +2,59 @@ require './test/test_helper'
 
 class OffsetTest < Minitest::Test
 
-  def test_it_can_find_todays_date
-    skip
+  def test_it_can_find_todays_date #unnecessary test?
     enigma = Enigma.new
+    today = Time.new
+
+    expected = ""
+    expected << today.month.to_s
+    expected << today.day.to_s
+    expected << today.year.to_s[-2,2]
+    enigma.format_to_char_length(expected, 6)
 
     assert_equal expected, enigma.todays_date
   end
 
-  end
-  def test_it_can_covert_todays_date_into_a_string
-  skip
+  def test_it_can_create_todays_date_as_a_valid_string
     enigma = Enigma.new
 
-    assert_equal "022319", enigma.todays_date_to_string
+    assert_instance_of String, enigma.todays_date
+  end
+
+  def test_it_can_format_date_to_six_characters_and_prepend_with_zero_if_needed
+    enigma_1 = Enigma.new
+    enigma_2 = Enigma.new
+    enigma_2.encrypt("hello world", "02715", "31718")
+
+    assert_equal 6, enigma_1.todays_date.length
+    assert_equal 6, enigma_2.encrypted[:date].length
+    assert_equal "031718", enigma_2.encrypted[:date]
   end
 
   def test_it_can_populate_todays_date_if_no_date_given
-  skip
     enigma = Enigma.new
     enigma.encrypt("hello world", "02715")
+
+    expected = enigma.todays_date
 
     assert_equal expected, enigma.encrypted[:date]
   end
 
-  # def test_it_can_convert_and_square_a_date_string_to_an_integer
-  #   enigma = Enigma.new
-  #   enigma.encrypt("hello world", "02715", "040895")
-  #
-  #   assert_equal
-  # end
+  def test_it_can_return_the_last_four_digits_of_the_date_value_squared
+    enigma = Enigma.new
+
+    assert_equal "1025", enigma.last_four_of_squared("040895")
+    assert_equal "3769", enigma.last_four_of_squared("111387")
+  end
 
   def test_it_has_four_different_offset_values_after_evaluating_key_argument
-    skip
+    enigma = Enigma.new
+    enigma.encrypt("hello world", "02715", "040895")
 
-    assert_equal, enigma.a_offset_values()
-    assert_equal, enigma.b_offset_values()
-    assert_equal, enigma.c_offset_values()
-    assert_equal, enigma.d_offset_values()
+    assert_equal 1, enigma.a_offset_values("040895")
+    assert_equal 0, enigma.b_offset_values("040895")
+    assert_equal 2, enigma.c_offset_values("040895")
+    assert_equal 5, enigma.d_offset_values("040895")
   end
 
 end
