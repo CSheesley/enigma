1cd269e6999734efbf2da72d13c649291cb76a2c
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-22 20:52:20 -0700

    Adds methods for each of the four key shift options (a,b,c,d)

--- lib/key.rb
+++ lib/key.rb
@@ -1,9 +1,4 @@
 module Key
-  attr_reader :characters
-
-  # def initialize(characters = random_characters )
-  #   @characters = format_to_five_chars(characters)
-  # end
 
   def format_to_five_chars(characters)
     until characters.length == 5
@@ -16,5 +11,21 @@ module Key
     options = ("0".."99999").to_a
     selection = options.sample
     format_to_five_chars(selection)
+  end
+
+  def a_key_values(key)
+    (key[0] + key[1]).to_i
+  end
+
+  def b_key_values(key)
+    (key[1] + key[2]).to_i
+  end
+
+  def c_key_values(key)
+    (key[2] + key[3]).to_i
+  end
+
+  def d_key_values(key)
+    (key[3] + key[4]).to_i
   end
 end
