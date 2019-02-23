db8e5ba4b828615a58f27f1241df00299b5ae032
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-22 19:05:33 -0700

    Adds pseupo code, details to possible methods and tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,6 +2,57 @@ require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
 
+<<<<<<< Updated upstream
+=======
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt_a_message_with_a_key_and_a_date
+    skip
+    enigma = Enigma.new
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_a_message_with_a_key_and_a_date
+    skip
+    enigma = Enigma.new
+    expected = {decryption: "hello world", key: "02715", date: "040895"}
+
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_a_message_with_a_key_and_no_date
+    skip
+    enigma = Enigma.new
+    # use today's date for date output in hash result - determine how to test
+    # include todays date in expected value
+    expected = {encryption: "keder ohulw", key: "02715"}
+
+    assert_equal expected, enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_decrypt_a_message_with_a_key_and_no_date
+    skip
+    enigma = Enigma.new
+    # use today's date for date output in hash result - determine how to test
+    # include todays date in expected value
+    expected = {encryption: "keder ohulw", key: "02715"}
+
+    assert_equal expected, enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
+  def test_it_can_encrypt_a_message_with_only_a_message
+    skip
+    enigma = Enigma.new
+    # use today's date for date output in hash result - determine how to test
+    # random key output? Test what we do know. Todays date, encrypted message,
+    # length of key > 5
+    expected = {encryption: "keder ohulw"}
+>>>>>>> Stashed changes
 
 
 end
