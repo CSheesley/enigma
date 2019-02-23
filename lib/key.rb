e0e1ec3e98a6e966eaa35b7755e031461b80ae45
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-23 10:14:51 -0700

    Reformats format_to_five_chars to take two arguments, allowing it to be more dynamic

--- lib/key.rb
+++ lib/key.rb
@@ -1,7 +1,7 @@
 module Key
 
-  def format_to_five_chars(characters)
-    until characters.length == 5
+  def format_to_char_length(characters, length)
+    until characters.length == length
       characters.prepend("0")
     end
     characters
@@ -10,7 +10,7 @@ module Key
   def random_characters
     options = ("0".."99999").to_a
     selection = options.sample
-    format_to_five_chars(selection)
+    format_to_char_length(selection, 5)
   end
 
   def a_key_values(key)
