c7766fef0db65bbf73f457785cb299bd9bef0e23
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-23 09:45:33 -0700

    Adds todays_date as an optional argument for encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,7 +10,7 @@ class Enigma
     @encrypted = {}
   end
 
-  def encrypt(phrase, key = random_characters, date = "111387")
+  def encrypt(phrase, key = random_characters, date = todays_date)
     encrypted[:encryption] = phrase
     encrypted[:key] = format_to_five_chars(key)
     encrypted[:date] = date
