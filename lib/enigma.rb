ab5c330c9f1481f8bd1e79169047315f78360c95
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-23 17:07:57 -0700

    Uses cipher class method to encode phrase when encrypt method called

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,6 +10,7 @@ class Enigma < Cipher
   end
 
   def encrypt(phrase, key = random_characters, date = todays_date)
+    cipher = Cipher.new(phrase, key, date)
     encrypted[:key] = format_to_char_length(key, 5)
     encrypted[:date] = format_to_char_length(date, 6)
     encrypted[:encryption] = cipher.encoded
