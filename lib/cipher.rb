517e2fec6ce3ac1cf5d2b0772b4103b848bb9e75
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-24 09:28:32 -0700

    Adds downcase method to phrase argument in initialization

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -5,7 +5,7 @@ class Cipher
     attr_reader :key, :date
 
   def initialize(phrase, key, date)
-    @phrase = phrase
+    @phrase = phrase.downcase
     @key = format_to_char_length(key, 5)
     @date = format_to_char_length(date, 6)
   end
