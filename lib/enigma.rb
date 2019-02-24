17a8a28e3e5eadec110200e75c1e6dcc599c7902
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-23 17:07:25 -0700

    Uses cipher class ofject to encode phrase when encrypt method called

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,18 @@
+require './lib/cipher'
 require './lib/shift'
 
-class Enigma
-  attr_accessor :encrypted
+class Enigma < Cipher
   include Shift
+  attr_accessor :encrypted
 
   def initialize
     @encrypted = {}
   end
 
   def encrypt(phrase, key = random_characters, date = todays_date)
-    encrypted[:encryption] = phrase
     encrypted[:key] = format_to_char_length(key, 5)
     encrypted[:date] = format_to_char_length(date, 6)
+    encrypted[:encryption] = cipher.encoded
     @encrypted
   end
 
