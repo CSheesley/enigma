11d236875b17eb9f1573ea46c156743a36402b5d
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-25 16:46:48 -0700

    Moves shift_table, encrypt_table, and decrypt_table methods to the shift module

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -14,29 +14,6 @@ class Cipher
     ("a".."z").to_a << " "
   end
 
-  def encrypt_table(key, date)
-    tables = {}
-    tables[:a] = shift_table(a_shift_value(key, date))
-    tables[:b] = shift_table(b_shift_value(key, date))
-    tables[:c] = shift_table(c_shift_value(key, date))
-    tables[:d] = shift_table(d_shift_value(key, date))
-    tables
-  end
-
-  def decrypt_table(key, date)
-    tables = {}
-    tables[:a] = shift_table(a_shift_value(key, date) * -1)
-    tables[:b] = shift_table(b_shift_value(key, date) * -1)
-    tables[:c] = shift_table(c_shift_value(key, date) * -1)
-    tables[:d] = shift_table(d_shift_value(key, date) * -1)
-    tables
-  end
-
-  def shift_table(shift_value)
-    shifted_characters = character_index.rotate(shift_value)
-    table = character_index.zip(shifted_characters).to_h
-  end
-
   def encoded
     encoded = ""
     split_message = @message.chars
