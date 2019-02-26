99137543663e2ea3faf08a607dcea1a7dbc004fc
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-25 20:05:51 -0700

    Adds decode a message and ignore \n test to cipher test

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -8,10 +8,16 @@ class CipherTest < Minitest::Test
     assert_equal "keder ohulw", cipher.encoded
   end
 
-  def test_it_can_encode_a_message_allows_special_characters_to_remain_unchanged
+  def test_it_can_encode_a_message_and_allow_special_characters_to_remain_unchanged
     cipher = Cipher.new("he((o w0r!d", "02715", "040895")
 
     assert_equal "ke((r o0u!w", cipher.encoded
+  end
+
+  def test_it_can_decode_a_message_and_ignore_newlines
+    cipher = Cipher.new("keder ohulw\n", "02715", "040895")
+
+    assert_equal "hello world", cipher.decoded
   end
 
   def test_it_coverts_capitalized_letter_to_lower_cased_letters
